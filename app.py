95da0d0adf485431677c01b4d6fa82ddd99bb0c7
Gabriel Rojo <gredztone@gmail.com>
2021-09-12 23:11:00 -0500

    Creando la funcion para listar solo las propiedades en pre_venta, en_venta y vendido

--- app.py
+++ app.py
@@ -1,22 +1,46 @@
-from flask import Flask
+from flask import Flask, jsonify,render_template, abort
 from flask_mysqldb import MySQL
-from config import config
 
 app = Flask(__name__)
 
-connection = MySQL(app)
+app.config['MYSQL_USER'] = "pruebas"
+app.config['MYSQL_PASSWORD'] = "VGbt3Day5R"
+app.config['MYSQL_HOST'] = "3.130.126.210"
+app.config['MYSQL_PORT'] = 3309
+app.config['MYSQL_DB'] = "habi_db"
 
+mysql = MySQL(app)
 
-@app.route('/test')
-
-def property_available():
+@app.route('/', methods=['GET'])
+def listar_propiedades_disponibles():
     try:
-        return "ok"
+        cursor = mysql.connection.cursor()
+        sql = "SELECT status_history.status_id, " \
+              "status.name," \
+              "property.address," \
+              "property.city," \
+              "property.price," \
+              "property.description," \
+              "property.year " \
+              "FROM status_history " \
+              "inner join status on status.id = status_history.status_id " \
+              "inner join property on status_history.property_id = property.id WHERE name='pre_venta' OR name='en_venta' OR name = 'vendido'"
+        cursor.execute(sql)
+        datos = cursor.fetchall()
+        propiedades=[]
+        for fila in datos:
+            curso={'Dirección':fila[2],'Ciudad':fila[3], 'Precio de Venta':fila[4], 'Descripcion':fila[5]}
+            propiedades.append(curso)
+        return jsonify({'propiedades':propiedades})
 
     except Exception as ex:
-        return "Error"
+        return jsonify({'mensaje':"error"})
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
 
 
 if __name__ == '__main__':
-    app.config.from_object(config['development'])
-    app.run()
+    app.run(Debug=True)
